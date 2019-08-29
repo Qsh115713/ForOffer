cce7c020d2ce779c898b9937a6ac37f31b5233cf
qsh <qsh0911@qq.com>
2019-08-29 20:44:52 +0800

    bug fix

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,6 +4,139 @@ import java.util.*;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        int x = 1, y = 1, c = 0;
+        c &= x | y;
+        System.out.println(c);
+    }
+
+    public boolean isValidBST2(TreeNode root) {
+        Stack<TreeNode> s = new Stack<>();
+        TreeNode pre = null, p = root;
+        while (p != null || !s.empty()) {
+            while (p != null) {
+                s.push(p);
+                p = p.left;
+            }
+            p = s.pop();
+            if (pre == null || pre.val < p.val) {
+                pre = p;
+            } else {
+                return false;
+            }
+            p = p.right;
+        }
+        return true;
+    }
+
+    public boolean isValidBST1(TreeNode root) {
+        return isValidBSTLoop(root, null, null);
+    }
+
+    private boolean isValidBSTLoop(TreeNode root, TreeNode minNode, TreeNode maxNode) {
+        if (root == null) return true;
+        if (minNode != null && root.val <= minNode.val || maxNode != null && root.val >= maxNode.val) return false;
+        return isValidBSTLoop(root.left, minNode, root) && isValidBSTLoop(root.right, root, maxNode);
+    }
+
+    public boolean exist(char[][] board, String word) {
+        if (board.length == 0 || board[0].length == 0) return false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (existLoop(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean existLoop(char[][] board, String word, int i, int j, int k) {
+        if (k == word.length()) return true;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(k)) {
+            return false;
+        }
+        board[i][j] ^= 256;
+        boolean b = existLoop(board, word, i - 1, j, k + 1) ||
+                existLoop(board, word, i + 1, j, k + 1) ||
+                existLoop(board, word, i, j - 1, k + 1) ||
+                existLoop(board, word, i, j + 1, k + 1);
+        board[i][j] ^= 256;
+        return b;
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        subsetsLoop(res, new ArrayList<>(), nums, 0);
+        return res;
+    }
+
+    private void subsetsLoop(List<List<Integer>> res, List<Integer> list, int[] nums, int index) {
+        res.add(new ArrayList<>(list));
+        for (int i = index; i < nums.length; i++) {
+            list.add(nums[i]);
+            subsetsLoop(res, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public String addBinary(String a, String b) {
+        int i = a.length(), j = b.length();
+        int x, y, z, c = 0;
+        StringBuilder res = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            x = i >= 0 ? a.charAt(i--) - '0' : 0;
+            y = j >= 0 ? b.charAt(j--) - '0' : 0;
+            z = x ^ y ^ c;
+            c &= x | y;
+            res.insert(0, z);
+        }
+        return res.toString();
+    }
+
+    public int[] plusOne(int[] digits) {
+        int i, c = 1, t;
+        int[] res;
+        for (i = 0; i < digits.length; i++) {
+            if (digits[i] != 9) break;
+        }
+        if (i >= digits.length) {
+            res = new int[digits.length + 1];
+            digits[0] = 1;
+            return res;
+        }
+        res = new int[digits.length];
+        for (i = res.length - 1; i >= 0; i--) {
+            t = digits[i] + c;
+            res[i] = t % 10;
+            c = t / 10;
+        }
+        return res;
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.equals("")) return "";
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = true;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
+            }
+        }
+        int len = n;
+        while (len-- >= 0) {
+            for (int i = 0; i < n - len; i++) {
+                if (dp[i][i + len]) {
+                    return s.substring(i, i + len + 1);
+                }
+            }
+        }
+        return "";
+    }
+
     public int maxPathSum(TreeNode root) {
         if (root == null) return 0;
         maxPathSumLoop(root);
